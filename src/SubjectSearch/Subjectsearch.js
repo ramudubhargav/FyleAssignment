f77d453820ba288c126567ac21780370def0c222
ramudubhargav <ramudu.bhargav@rediffmail.com>
2023-03-05 01:00:55 +0530

    Added back button

--- src/SubjectSearch/Subjectsearch.js
+++ src/SubjectSearch/Subjectsearch.js
@@ -27,11 +27,15 @@ function Subjectsearch() {
     const inp = document.getElementById("val").value;
     setsearch(inp);
   }
+  function handleclick(){
+    setsearch("");
+  }
   return (
     <div className="toolbar">
       <div className="input-button-container">
         <Input  id="val"  className="subsearch-input" placeholder="Search by Subject" />
-        <Button onClick={handlebutton} className="subsearch-btn" type="primary">Search</Button>
+        <Button onClick={handlebutton} className="subsearch-btn" type="primary" style={{marginRight: '10px'}}>Search</Button>
+        <Button disabled={search.length==0}onClick={handleclick} className="subsearch-btn" type="primary">Back</Button>
         <hr />
       </div>
       {search.length == 0 ? <Display /> : <Subsearch inp={search} />}
